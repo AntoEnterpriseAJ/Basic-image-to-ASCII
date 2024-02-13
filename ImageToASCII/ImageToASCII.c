bb23ef163ddda4db9e67a633d86debbf8a16e06a
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-02-13 23:00:10 +0200

    added checls

--- ImageToASCII/ImageToASCII.c
+++ ImageToASCII/ImageToASCII.c
@@ -9,6 +9,13 @@ int main(int argc, char** argv)
 {
 	int width, height, channels;
 	char* image = stbi_load("imageName", &width, &height, &channels, 0);
+	if (image == NULL)
+	{
+		perror("Error loading the image");
+		return 1;
+	}
 
+
+	stbi_image_free(image);
 	return 0;
 }
